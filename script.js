1e362ed71867600bb809f4d8e82ea4c07e8abd06
hariswahid123 <hariswahid2010@gmail.com>
2025-08-06 16:16:27 +0500

    many changes

--- script.js
+++ script.js
@@ -11,33 +11,30 @@ let dice5 = "<div class=container><div class=special></div><div></div><div  clas
 
 let dice6 = "<div class=container><div class=special></div><div class=special></div><div class=special></div><div ></div><div></div><div ></div><div class=special></div><div class=special></div><div class=special></div></div>"
 
-let resultDice = document.getElementById("resultDice"); 
+let resultDice = document.getElementById("resultDice");
 
 
-function play () {
-    let diceRoll = Math.ceil(Math.random() * 6);
+function play() {
+   let diceRoll = Math.ceil(Math.random() * 6);
 
-    if(diceRoll === 1){
-       resultDice.innerHTML = dice1;
-    }else if(diceRoll === 2){
-       resultDice.innerHTML = dice2;
-    }else if(diceRoll === 3){
-       resultDice.innerHTML = dice3;
-    }else if(diceRoll === 4){
-       resultDice.innerHTML = dice4;
-    }else if(diceRoll === 5){
-       resultDice.innerHTML = dice5;
-    }else if(diceRoll === 6){
-       resultDice.innerHTML = dice6;
-       alert("you are lucky")
-    }
+
+   if (diceRoll === 1) {
+      resultDice.innerHTML = dice1;
+   } else if (diceRoll === 2) {
+      resultDice.innerHTML = dice2;
+   } else if (diceRoll === 3) {
+      resultDice.innerHTML = dice3;
+   } else if (diceRoll === 4) {
+      resultDice.innerHTML = dice4;
+   } else if (diceRoll === 5) {
+      resultDice.innerHTML = dice5;
+   } else if (diceRoll === 6) {
+      resultDice.innerHTML = dice6;
+   }
+
+
 };
 
-let balance = document.getElementById("balance");
-
-balance = 1000;
-
-console.log(balance)
 
 
 
